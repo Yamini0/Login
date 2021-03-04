094b47d215da1a7276af4c10641636cf67e80057
Yamini0 <yaminis341@gmail.com>
2021-03-04 20:41:06 +0530

    Implementation of Tab Navigation

--- Navigation.js
+++ Navigation.js
@@ -1,80 +1,99 @@
-//import "react-native-gesture-handler";
+import "react-native-gesture-handler";
 import React from "react";
-import { Text, View, Image } from "react-native";
-//import { NavigationContainer } from "@react-navigation/native";
-//import { createStackNavigator } from "@react-navigation/stack";
-import { createAppContainer } from "react-navigation";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-
+import { createDrawerNavigator } from "react-navigation-drawer";
 import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
-//import { createDrawerNavigator } from "@react-navigation/drawer";
 
+//importing Screens
 import Login1 from "./screens/Login1";
 import ProfileScreen from "./screens/ProfileScreen";
 import SettingScreen from "./screens/SettingScreen";
+import CoachScreen from "./screens/CoachScreen";
+import ExploreScreen from "./screens/ExploreScreen";
 
-const AppNavigator = createStackNavigator(
-  {
-    Login: {
-      screen: Login1,
-      navigationOptions: {
-        tabBarLabel: "Login Page",
-        color: "#1A5CAD",
-        //tabBarActiveTintColor
-      },
-    },
-    ProfileScreen: {
-      screen: ProfileScreen,
-      navigationOptions: {
-        tabBarLabel: "ProfileScreen",
-        color: "#1A5CAD",
-        //tabBarActiveTintColor
-      },
+const AppStack = createStackNavigator({
+  Login: {
+    screen: Login1,
+    navigationOptions: {
+      tabBarLabel: "Login Page",
+      color: "#1A5CAD",
+      //tabBarActiveTintColor
     },
   },
-  {
-    tabBarOptions: {
-      activeTintColor: "#74442D",
-      inactiveTintColor: "grey",
-      style: {
-        backgroundColor: "white",
-      },
+  ProfileScreen: {
+    screen: ProfileScreen,
+    navigationOptions: {
+      tabBarLabel: "ProfileScreen",
+      color: "#1A5CAD",
+      //tabBarActiveTintColor
     },
+  },
+  SettingScreen: {
+    screen: SettingScreen,
+    navigationOptions: {
+      tabBarLabel: "ProfileScreen",
+      color: "#1A5CAD",
+      //tabBarActiveTintColor
+    },
+  },
+  CoachScreen: {
+    screen: CoachScreen,
+    navigationOptions: {
+      tabBarLabel: "ProfileScreen",
+      color: "#1A5CAD",
+      //tabBarActiveTintColor
+    },
+  },
+});
 
-    initialRouteName: "Login",
-  }
-);
-
-const BottonTabPage = createMaterialBottomTabNavigator(
+const BottomTabPage = createMaterialBottomTabNavigator(
   {
-    Login: { screen: Login1 },
-    ProfileScreen: { screen: ProfileScreen },
-    SettingScreen: { screen: SettingScreen },
+    Profile: { screen: AppStack },
+    Setting: { screen: SettingScreen },
+    Coach: { screen: CoachScreen },
+    Explore: { screen: ExploreScreen },
+
+    // SettingScreen: { screen: SettingScreen },
   },
   {
-    initialRouteName: "ProfileScreen",
     activeColor: "#f0edf6",
     inactiveColor: "#3e2465",
     barStyle: { backgroundColor: "#694fad" },
   }
 );
-//const Drawer = createDrawerNavigator();
 
-export default createAppContainer(AppNavigator, BottonTabPage);
+const AppNavigator = createSwitchNavigator({
+  AppStack: {
+    screen: BottomTabPage,
+    path: "onBoarding",
+  },
+});
+
+/*const AppDrawerNavigator = createDrawerNavigator({
+  Profile: ProfileScreen,
+  setting: SettingScreen,
+});*/
+export default createAppContainer(AppNavigator);
+
+//const AppNavigator = createStackNavigator();
 {
   /*
-
-const AppNavigator = createStackNavigator();
-
 export default function Navigation() {
   return (
-    <NavigationContainer>
+    /*<NavigationContainer>
       <AppNavigator.Navigator headerMode="none">
         <AppNavigator.Screen name="Login" component={Login1} />
         <AppNavigator.Screen name="ProfileScreen" component={ProfileScreen} />
       </AppNavigator.Navigator>
     </NavigationContainer>
-  );
-}
-*/
+
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Login">
+        <Drawer.Navigator name="login" component={ProfileScreen} />
+        <Drawer.Navigator name="profile" component={ProfileScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  ),
+  */
 }
