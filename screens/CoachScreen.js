36491cd94a737d1e2ea89ce73bfbf7509d9d3ae9
Yamini0 <yaminis341@gmail.com>
2021-03-25 19:07:46 +0530

    Updated CoachScreen

--- screens/CoachScreen.js
+++ screens/CoachScreen.js
@@ -1,15 +1,45 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
-
-import Carousel from "../component/Carousel";
-import { dummyData } from "../apis/Data";
-
-export function CoachScreen({ navigation }) {
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  ImageBackground,
+  Image,
+  TextInput,
+  Dimensions,
+  TouchableOpacity,
+  Alert,
+  StyleSheet,
+  Text,
+  Animated,
+  Easing,
+} from "react-native";
+import logo from "../assets/atom.png";
+import LogoAnimation from "../component/LogoAnimation";
+// import * as Font from 'expo-font';
+const CoachScreen = ({ navigation }) => {
   return (
-    <SafeAreaView>
-      <Carousel data={dummyData} />
-    </SafeAreaView>
+    <ImageBackground style={styles.backgroundContainer}>
+      <View style={styles.LogoContainer}>
+        <LogoAnimation />
+        <Text style={styles.LogoText}>ExploreScreeen</Text>
+      </View>
+    </ImageBackground>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  backgroundContainer: {
+    flex: 1,
+    width: null,
+    height: null,
+    justifyContent: "center",
+    alignItems: "center",
+    opacity: 0.6,
+    backgroundColor: "#fafaff",
+  },
+  LogoContainer: {
+    alignItems: "center",
+    marginBottom: 50,
+  },
+});
 
 export default CoachScreen;
