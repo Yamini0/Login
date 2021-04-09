12642aff1c4bc8be7922edc2eda973be983d9c6f
Yamini0 <yaminis341@gmail.com>
2021-04-09 15:50:24 +0530

    Updated Json

--- apis/FoodData.js
+++ apis/FoodData.js
@@ -1,13 +1,36 @@
+import headerImg from "../images/cherry0.png";
+import productImg1 from "../images/cherry1.jpg";
+import productImg2 from "../images/cherry2.jpg";
+import productImg3 from "../images/cherry3.jpg";
+
 export const FoodData = {
-  Title: "Australian Cherry",
-  OfferPrice: "Rs.2450",
-  Price: "Rs.2500",
-  Body:
+  title: "Australian Cherry",
+  offerPrice: "Rs.2450",
+  price: "Rs.2500",
+  image: headerImg,
+  description:
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy textever since the 1500s, when an unknown printer took a galley of typeand scrambled it to make a type specimen book. It has survived notonly five centuries, but also the leap into electronic typesetting,remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,and more recently with desktop publishing software like Aldus",
-  Url1: require("../images/cherry1.jpg"),
-  Url2: require("../images/cherry2.jpg"),
-  Url3: require("../images/cherry3.jpg"),
-  Name1: "Fresh Cherry",
-  Name2: "Japan Cherry",
-  image: require("../images/cherry0.png"),
+
+  products: [
+    {
+      productId: "1",
+      productName: "American Cherry",
+      productImg: productImg1,
+    },
+    {
+      productId: "2",
+      productName: "Japanesse Cherry",
+      productImg: productImg2,
+    },
+    {
+      productId: "3",
+      productName: "Indian Cherry",
+      productImg: productImg3,
+    },
+    {
+      productId: "4",
+      productName: "Indian Cherry",
+      productImg: headerImg,
+    },
+  ],
 };
